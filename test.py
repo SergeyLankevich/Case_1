8885a71267a6a1b9fec7888586e92c076729e3cc
zaitsvnast <cenu_m@mail.ru>
2019-09-17 14:07:22 +0700

    Test push

---
+++ test.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle.fd(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.rt(90)
+turtle.fd(100)
+turtle.right(90)
+turtle.fd(100)
+turtle.fd(100)
+input('Press ENTER to exit')
