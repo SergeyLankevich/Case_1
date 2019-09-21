72a1aa0c1022da3f2d47331c70677faa08ecbf73
Илья <malakhov.ilya.pavlovich@yandex.ru>
2019-09-21 22:34:51 +0700

    Added functions: sun, tree

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def triangle_1( edge, turn, color, x = 0, y = 0):
     turtle.end_fill()
 
     """
-    Keyword argument: 
+    Done: Ilya
+    Keyword arguments: 
     edge - lenght of cathet
     turn - turn of triangle at clockwise
     color - fill color of triangle
@@ -44,7 +45,8 @@ def rectangle( edgex, edgey, color, x = 0, y = 0):
     turtle.end_fill()
 
     """
-    Keyword argument: 
+    Done: Ilya
+    Keyword arguments: 
     edgex - lenght x
     edgey - lenght y
     turn - turn of triangle at clockwise
@@ -128,7 +130,8 @@ def house(x = 0, y = 0):
     rectangle(6, 50, '#2F1400', x + 97, y + 75)
 
     """
-    Keyword argument: 
+    Done: Ilya
+    Keyword arguments: 
     x - x coordinate
     y - y coordinate
     """
@@ -136,14 +139,57 @@ def house(x = 0, y = 0):
 
 
 def sun(x = 0, y = 0):
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.setheading(0)
+    turtle.down()
+    turtle.color('#FFDB00')
+
+    turtle.begin_fill()
+    turtle.circle(25)
+    turtle.end_fill()
+
+    angle = 5.74
+    turtle.up()
+    turtle.setposition(x, y + 30)
+    turtle.setheading(0)
+    turtle.down()
+
+    turtle.begin_fill()
+    turtle.fd(50)
+    for i in range(36):
+        turtle.rt(175)
+        turtle.fd(100)
+
+    turtle.end_fill()
 
 
 
-def picture_3():
     """
-    #TODO: Painting the first picture using functions above: Ilya
-    Keyword argument: 
+    Done: Ilya
+    Keyword arguments:
+    x - x coordinate
+    y - y coordinate
+    """
 
+
+def tree(x = 0 , y = 0):
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.setheading(0)
+    turtle.down()
+
+    rectangle(30, 150, '#502400', x, y)
+    rectangle(200, 175, '#006500', x - 85, y + 150)
+    rectangle(50, 15, '#502400', x + 30, y + 100)
+    rectangle(15, 35, '#502400', x + 65, y + 115)
+    rectangle(150, 150, '#006500', x - 20, y + 190)
+
+    """
+    Done: Ilya
+    Keyword arguments:
+    x - x coordinate
+    y - y coordinate
     """
 
 def picture_4():
@@ -198,6 +244,9 @@ def picture_9():
 
 ###############################################################################################
 
-house()
+turtle.speed(0)
+house(-100, -100)
+sun(250, 200)
+tree(-400, -100)
 
 input('Press ENTER to exit')
