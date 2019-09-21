54785062be7acc731bbf51dadfec9879f2d82883
SergeyLankevich <43179675+SergeyLankevich@users.noreply.github.com>
2019-09-21 18:54:54 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,145 @@
+from turtle import *
+from math import *
+
+
+def triangle_1():
+    """
+    #TODO: Painting a triangle: Ilya
+    Keyword argument: 
+
+    """
+
+
+def rectangle():
+    """
+    #TODO: Painting a triangle: Ilya
+    Keyword argument: 
+
+    """
+
+
+def additional_figure_1():
+    """
+    #TODO: Paint and additional figure if needed: Ilya
+    Keyword argument: 
+
+    """
+
+
+def additional_figure_2():
+    """
+    #TODO: Paint and additional figure if needed: Ilya
+    Keyword argument: 
+
+    """
+
+
+
+def additional_figure_3():
+    """
+    #TODO: Paint and additional figure if needed: Ilya
+    Keyword argument: 
+
+    """
+
+
+def additional_figure_4():
+    """
+    #TODO: Paint and additional figure if needed: Sergey
+    Keyword argument: 
+
+    """
+
+
+def additional_figure_5():
+    """
+    #TODO: Paint and additional figure if needed: Sergey
+    Keyword argument: 
+
+    """
+
+
+def additional_figure_6():
+    """
+    #TODO: Paint and additional figure if needed: Sergey
+    Keyword argument: 
+
+    """
+
+
+def additional_figure_7():
+    """
+    #TODO: Paint and additional figure if needed: Sergey
+    Keyword argument: 
+
+    """
+
+def picture_1():
+    """
+    TODO: Painting the first picture using functions above: Ilya
+    Keyword argument: 
+
+    """
+
+def picture_2():
+    """
+    #TODO: Painting the first picture using functions above: Ilya
+    Keyword argument: 
+
+    """
+def picture_3():
+    """
+    #TODO: Painting the first picture using functions above: Ilya
+    Keyword argument: 
+
+    """
+
+def picture_4():
+    """
+    #TODO: Painting the first picture using functions above: Ilya
+    Keyword argument: 
+
+    """
+
+
+def picture_5():
+    """
+    #TODO: Painting the first picture using functions above: Sergey
+    Keyword argument: 
+
+    """
+
+
+def picture_6():
+    """
+    #TODO: Painting the first picture using functions above: Sergey
+    Keyword argument: 
+
+    """
+
+
+def picture_7():
+    """
+    #TODO: Painting the first picture using functions above: Sergey
+    Keyword argument: 
+
+    """
+
+
+
+def picture_8():
+    """
+    #TODO: Painting the first picture using functions above: Sergey
+    Keyword argument: 
+
+    """
+
+
+
+def picture_9():
+    """
+    #TODO: Painting the first picture using functions above: Sergey
+    Keyword argument: 
+
+    """
+
